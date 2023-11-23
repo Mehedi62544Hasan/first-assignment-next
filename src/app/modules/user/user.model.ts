e5e7c8ba99f024f62cf98bb155beb358832ec32e
Mehedi62544Hasane <mehedi62544hasan@gmail.comm>
2023-11-23 19:16:27 +0000

    make controller done

---
+++ src/app/modules/user/user.model.ts
@@ -0,0 +1,59 @@
+import { Schema, model } from 'mongoose';
+import { User } from './user.interface';
+
+const useUserSchema = new Schema<User>({
+  userId: {
+    type: Number,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  fullName: {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+  },
+  age: {
+    type: Number,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  isActive: {
+    type: Boolean,
+    default: true,
+  },
+  hobbies: {
+    type: [String],
+    default: [],
+  },
+  address: {
+    street: {
+      type: String,
+      required: true,
+    },
+    city: {
+      type: String,
+      required: true,
+    },
+    country: {
+      type: String,
+      required: true,
+    },
+  },
+});
+
+export const UserModel = model<User>('User', useUserSchema);
